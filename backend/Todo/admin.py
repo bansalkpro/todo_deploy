a0c23660286ee038f713819c9c2c8b78b43b3824
bansalkpro <bansalk.pro@gmail.com>
2024-09-26 20:25:03 +0530

    first commit

---
+++ backend/Todo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Todo
+# Register your models here.
+# create a class for the admin-model integration
+class TodoAdmin(admin.ModelAdmin):
+    # add the fields of the model here
+    list_display = ("title","description","completed")
+# we will need to register the model class and the Admin model class
+# using the register() method  of admin.site class
+admin.site.register(Todo,TodoAdmin)
